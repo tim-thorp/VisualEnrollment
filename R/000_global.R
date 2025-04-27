c7782807a4e49a5da3111464424f7e6174c74905
Tim Thorp <tthorp@uoc.edu>
2025-04-28 00:42:31 +0200

    Delete redundant functionality from `zzz.R` and move remaining code into `000_global.R`

--- R/000_global.R
+++ R/000_global.R
@@ -1,2 +1,8 @@
 globalVariables(c("subject_distance_matrix", "subject_positions"))
 
+# Make inst/www directory available as www
+shiny::addResourcePath(
+  prefix = "www",
+  directoryPath = system.file("www", package = "visualenrollment")
+)
+
