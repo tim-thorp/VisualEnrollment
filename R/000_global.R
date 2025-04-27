b168253d5bf8fdbe929353678e4f227564c0eac3
Tim Thorp <tthorp@uoc.edu>
2025-04-28 01:20:29 +0200

    Revert "Delete redundant functionality from `zzz.R` and move remaining code into `000_global.R`"
    This reverts commit c7782807a4e49a5da3111464424f7e6174c74905.

--- R/000_global.R
+++ R/000_global.R
@@ -1,8 +1,2 @@
 globalVariables(c("subject_distance_matrix", "subject_positions"))
 
-# Make inst/www directory available as www
-shiny::addResourcePath(
-  prefix = "www",
-  directoryPath = system.file("www", package = "visualenrollment")
-)
-
